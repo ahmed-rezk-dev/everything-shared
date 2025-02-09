47c523190a632cb906dc248047aeb29fec4ea3e2
Ahmed Rezk <rezk5024@gmail.com>
2025-02-09 14:47:03 -0700

    input component stories

--- packages/components/src/input/input.stories.tsx
+++ packages/components/src/input/input.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Input } from './input';
 
 const meta = {
-  title: 'MyButton',
+  title: 'Components/Input',
   component: Input,
   argTypes: {
     placeholder: { control: 'text' },
